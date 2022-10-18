fde9f79362f162b8a4f3035179c5bc5abadaaf7c
Alvaro <alvarojg27@gmail.com>
2022-10-17 22:15:42 -0500

    add central app
    add central operation from the app

---
+++ app/src/main/java/com/example/socialfilmapp/CentralActivity.kt
@@ -0,0 +1,11 @@
+package com.example.socialfilmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CentralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_central)
+    }
+}
